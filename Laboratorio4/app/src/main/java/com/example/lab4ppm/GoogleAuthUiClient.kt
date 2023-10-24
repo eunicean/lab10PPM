e3421e25720f414f0084af06a20599626a4e19f1
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-10-24 10:13:35 -0600

    Sign in functions done

--- Laboratorio4/app/src/main/java/com/example/lab4ppm/GoogleAuthUiClient.kt
+++ Laboratorio4/app/src/main/java/com/example/lab4ppm/GoogleAuthUiClient.kt
@@ -58,6 +58,24 @@ class GoogleAuthUiClient(
         }
     }
 
+    suspend fun signOut(){
+        try {
+            oneTapClient.signOut().await()
+            auth.signOut()
+        }catch (e: Exception){
+            e.printStackTrace()
+            if(e is CancellationException) throw e
+        }
+    }
+
+    fun getSignInUser(): UserData? = auth.currentUser?.run {
+        UserData(
+            userId = uid,
+            username = displayName,
+            profilePictureUrl = photoUrl?.toString()
+        )
+    }
+
     private fun buildSignInRequest(): BeginSignInRequest{
         return BeginSignInRequest.Builder()
             .setGoogleIdTokenRequestOptions(
---
+++ Laboratorio4/app/src/main/java/com/example/lab4ppm/SignInState.kt
@@ -0,0 +1,6 @@
+package com.example.lab4ppm
+
+data class SignInState(
+    val isSignSuccessful: Boolean = false,
+    val signInErrorMessage: String? = null
+)
